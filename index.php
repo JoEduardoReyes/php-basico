dcb3891017c15aa3ed63817524e08a89cd51a88d
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-19 17:32:36 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// imprimir
+echo "Holi, esto esta hecho en php \n";
+
+// concatenate
+
+$nombre = "Eduardo";
+$apellido = "Reyes";
+
+echo "No, no. Yo me llamo " . $nombre . " " . $apellido. "\n";
+
+echo "No, no. Yo me llamo $nombre $apellido \n";
+
+echo "El resultado de 5 x 5 es " . 4 * 5;
