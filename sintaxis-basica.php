92e34addf61ddd182ecc73c70a2ade3bc2e8d9bf
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-19 18:11:05 -0600

    Variables y constantes

---
+++ sintaxis-basica.php
@@ -0,0 +1,31 @@
+<?php
+
+// imprimir
+// echo "Holi, esto esta hecho en php \n";
+
+// // concatenate
+
+// $nombre = "Eduardo";
+// $apellido = "Reyes";
+
+// echo "No, no. Yo me llamo " . $nombre . " " . $apellido. "\n";
+
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+
+// echo "El resultado de 5 x 5 es " . 4 * 5;
+
+$personas = [
+  "Carlos" => 22, "Mr Michi" => 15, "Juan" => 65
+];
+
+// var_dump($personas);
+
+// print_r($personas);
+
+#Esto es un compentario
+
+/* Esto es otro comentario
+
+sdadasd
+
+*/
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+
+// VARIABLES
+$numero_1 = 8;
+$numero_2 = 7;
+
+echo $numero_1 + $numero_2;
+
+// CONSTANTES
+define("NUMERO_PI", 3.1416);
+
+echo NUMERO_PI;
