437da852b8cab9e19c861f257f8d541e5e78196a
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-19 18:19:42 -0600

    Tipos de datos

---
+++ tipos.php
@@ -0,0 +1,22 @@
+<?php
+
+// // string
+$numerito = "23";
+
+// integer
+$nuevo_numerito = $numerito + 2;
+
+var_dump($numerito);
+var_dump($nuevo_numerito);
+
+// float
+$numerito = 10;
+$numerito = $numerito + 0.5;
+
+var_dump($numerito);
+
+// string
+$papas = "10 papas en el costal";
+$cuantas_papas_hay = $papas + 10;
+
+echo $cuantas_papas_hay;
