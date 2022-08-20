37a50507ea0d9a3778e15d61f0db0bb94139e513
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-19 18:45:16 -0600

    Casting Reto

--- casting.php
+++ casting.php
@@ -18,3 +18,25 @@ $banderita = 1;
 $banderita = (bool) $banderita;
 
 var_dump($banderita);
+
+// reto 
+
+//Fácil
+
+$nombre = "Carlos"; // string
+$apellido = "Gómez"; // string
+$edad = 18; // integer
+$aprobado = true;  // boolean
+
+//Medio
+
+$promedio = (8 + 9.5 + 9 + 10 + 8) / 5; // integer
+$nombre_completo = $nombre . " " . $apellido;  // string
+$presento_examen = (bool) 1; // boolean
+
+//Avanzado
+
+$numero_preguntas = 5 + "5"; // number
+$numero_respuestas = "5" + 5; // string
+$promedio_maximo = $numero_respuestas / 1.0; // integer
+$michis = 3 + "5 michis"; // string
