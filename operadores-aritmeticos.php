94d13f4bceea39ff5b004c6902e67096f0d3aae6
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-24 17:31:45 -0600

    Operadores aritmeticos

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,14 @@
+<?php
+
+// $resultado = 5 ** 3;
+// echo $resultado;
+
+// echo (5 * 6) . ' ' . (80 / 4);
+
+// echo 5 / 2 . "\n";
+// echo 5 % 2;
+
+$horas = 7501;
+
+echo "7200 segundos convertidos a horas son " . ($horas / 3600) . " horas \n";
+echo "Sobran " . ($horas % 3600) . " segundos";
