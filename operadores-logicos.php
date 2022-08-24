1cfedcab2aa726cffcee0f2a67b507d070ccc817
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-24 17:04:08 -0600

    Tablas de verdad

---
+++ operadores-logicos.php
@@ -0,0 +1,27 @@
+<?php
+
+// $michis_felinos = true;
+// $michis_4_patas = true;
+// $michis_vuelan = false;
+// $michis_programan_con_PHP = false;
+
+// var_dump($michis_felinos && $michis_4_patas); true
+// var_dump($michis_4_patas || $michis_vuelan); true
+// var_dump( !$michis_4_patas );
+
+// $resultado = $michis_4_patas and $michis_programan_con_PHP;
+
+// var_dump($resultado);
+
+// # Reto
+
+$es_un_michi_grande = true;
+$le_gusta_comer = true;
+$sabe_volar = false;
+$tiene_2_patas = false;
+
+var_dump($es_un_michi_grande && $le_gusta_comer);
+var_dump($es_un_michi_grande || $sabe_volar);
+var_dump($sabe_volar || $tiene_2_patas);
+var_dump(!$le_gusta_comer);
+var_dump(!$le_gusta_comer || $es_un_michi_grande);
