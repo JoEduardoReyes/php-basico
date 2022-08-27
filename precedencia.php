26203bb40095e390e30c707454860f66026cf854
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-26 20:05:11 -0600

    Precedencia

---
+++ precedencia.php
@@ -0,0 +1,13 @@
+<?php
+
+// $contador = 1;
+// $resultado = $contador++;
+
+// var_dump($resultado);
+
+$michis_4_patas = true;
+$michis_programan_con_PHP = false;
+
+$resultado = ($michis_4_patas and $michis_programan_con_PHP);
+
+var_dump($resultado);
