b140aa970672bfb9e06d3fc6eb3412a2a99430f3
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-26 19:44:13 -0600

    otros operadores

---
+++ otros-operadores.php
@@ -0,0 +1,40 @@
+<?php
+
+// = operador de asignacion
+
+// $dead_de_jaimito = ($edad_de_pepito = 18) +5;
+
+// var_dump($dead_de_jaimito);
+// var_dump($edad_de_pepito);
+
+// + incremento - resta
+
+// $contador = 1;
+// // $contador = $contador + 1;
+// // $contador += 1;
+// $contador++;
+
+// var_dump($contador);
+
+/* 
+
++=
+-=
+*=
+/=
+
+*/ 
+
+
+// . concatenacion
+
+// $nombre = "carlos";
+// $nombre .= " " . "Santana";
+
+// var_dump($nombre);
+
+$contador = 1;
+$resultado = $contador++;
+
+var_dump($contador);
+var_dump($resultado);
